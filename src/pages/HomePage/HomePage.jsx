f564aa68b891e9a04f6590fb9b9ba69645f342dd
Abdullah2000Habib <abd3habib@gmail.com>
2023-02-06 16:48:41 +0200

    Chart Component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Chart from '../../components/Chart/Chart';
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo';
+import { userData } from './Data';
 import { HomeWrapper } from './style';
 
 export default function HomePage() {
@@ -7,6 +9,7 @@ return (
     <>
         <HomeWrapper>
             <FeaturedInfo/>
+            <Chart data={userData} title={"user Analytics"} dataKey={"Active User"}  grid/>
         </HomeWrapper>
     </>
 )
