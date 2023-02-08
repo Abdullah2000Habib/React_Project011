bfcb09eac44ed989b765744a4f6b151a1b3e1797
Abdullah2000Habib <abd3habib@gmail.com>
2023-02-08 15:51:01 +0200

    UserPage

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,15 @@
 import HomePage from "../pages/HomePage";
 import UserListPage from "../pages/UserListPage";
+import UserPage from "../pages/UserPage";
 
 export const  PATHS = {
     HOME :'/',
     USERlIST :'/users',
+    USER :'/user/:userId',
 };
 
 export const router = [
     {index:true,element:<HomePage/>},
     {path:PATHS.USERlIST ,element:<UserListPage/>},
+    {path:PATHS.USER ,element:<UserPage/>},
 ];
