7299ed408890a253ea01d252b53230fbd7095d61
Abdullah2000Habib <abd3habib@gmail.com>
2023-02-08 14:18:25 +0200

    USerListPage and Data table From  MUI Library and install @mui/x-data-grid And Router Dom Whit new way

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import MainStyle from './MainStyle';
 import Home from './components/Home';
-
 function App() {
   return (
     <>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,19 @@
-
 import React from 'react';
-import HomePage from '../pages/HomePage';
+import { useRoutes } from 'react-router-dom';
+import { router } from '../router';
 import Container from './Container';
 import Sidebar from './Sidebar/Sidebar';
 import Topbar from './Topbar/Topbar';
 
+
 export default function Home() {
+const routes = useRoutes(router);
 return (
     <>
         <Topbar/>
         <Container>
             <Sidebar />
-            <HomePage />
+            {routes}
         </Container>
     </>
 )
